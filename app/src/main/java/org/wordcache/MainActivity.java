1b7360b580d13540a3d34cddb160187a646c6598
v-chavezluis <chave232@alumni.umn.edu>
2025-01-15 17:06:08 -0800

    Imported the correct toolbar class. Application works now.

--- app/src/main/java/org/wordcache/MainActivity.java
+++ app/src/main/java/org/wordcache/MainActivity.java
@@ -3,22 +3,48 @@ package org.wordcache;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        //System.out.println("The call returns: " + findViewById(R.id.toolbar).getClass().getName());
+
+
+        // Toolbar
+        setSupportActionBar(toolbar);
+
+        // Navigation Drawer Menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drw_open, R.string.nav_drw_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
